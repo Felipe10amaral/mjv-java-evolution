86aedebc1f63c50e807cef1359fadbf2814d7540
Felipe Amaral <felipeamaraldev15@gmail.com>
2022-10-05 23:47:06 -0300

    projeto com o maven

---
+++ maven/controle-financeiro-app/src/main/java/org/digytal/ContaBancaria.java
@@ -0,0 +1,10 @@
+package org.digytal;
+
+import java.nio.file.FileSystemNotFoundException;
+
+public class ContaBancaria {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+
+}
