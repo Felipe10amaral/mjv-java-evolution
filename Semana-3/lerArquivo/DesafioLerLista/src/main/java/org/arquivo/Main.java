d209e77f0f52cd94501b134cf835684f5a8356ee
Felipe Amaral <felipeamaraldev15@gmail.com>
2022-10-16 15:08:25 -0300

    adicionando comentarios

--- Semana-3/lerArquivo/DesafioLerLista/src/main/java/org/arquivo/Main.java
+++ Semana-3/lerArquivo/DesafioLerLista/src/main/java/org/arquivo/Main.java
@@ -13,14 +13,15 @@ public class Main {
 
         try {
             sc = new Scanner(file);
-            while (sc.hasNextLine()) {
-                System.out.println(sc.nextLine());
+            while (sc.hasNextLine()) { // esse while testa para ve se ainda existe uma nova linha no arquivo
+                System.out.println(sc.nextLine());  // se existir uma linha ele vai imprimir na tela
             }
         }
         catch (IOException e) {
             System.out.println("Erro:" + e.getMessage());
         }
         finally {
+            // condicional para testar se o sc esta nulo para evitar uma null pointer exception
             if(sc != null) {
                 sc.close();
             }
