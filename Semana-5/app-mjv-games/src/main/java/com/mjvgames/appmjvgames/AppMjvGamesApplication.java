154de70c2f3e8599319f28bdc7690b042394d391
Felipe Amaral <felipeamaraldev15@gmail.com>
2022-11-01 00:07:25 -0300

    criando o projeto

---
+++ Semana-5/app-mjv-games/src/main/java/com/mjvgames/appmjvgames/AppMjvGamesApplication.java
@@ -0,0 +1,13 @@
+package com.mjvgames.appmjvgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMjvGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppMjvGamesApplication.class, args);
+	}
+
+}
---
+++ Semana-5/app-mjv-games/src/test/java/com/mjvgames/appmjvgames/AppMjvGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjvgames.appmjvgames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMjvGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
